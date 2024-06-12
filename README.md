e17a35253b2714a23f90d3cb7544c744ef327c60
Khajabasha Shaik <shaikkhajabasha3@gmail.com>
2024-06-12 22:47:42 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Voting-Mangement-system
+ Voting-Mangement-system using Full stack
---
+++ Voting Mangement system/db.php
@@ -0,0 +1,15 @@
+<?php
+// Database connection parameters
+$servername = "localhost"; // Change this to your database server address
+$username = "root"; // Change this to your database username
+$password = "Root"; // Change this to your database password
+$database = "vms"; // Change this to your database name
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
